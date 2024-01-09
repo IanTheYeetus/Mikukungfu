1a5597ab611aa07618caad65d041ca524977662e
Yeetus <75418633+IanTheYeetus@users.noreply.github.com>
2024-01-09 20:53:48 +0100

    Create miko.py

---
+++ miko.py
@@ -0,0 +1,22 @@
+from easygame import *
+
+open_window('Epic Gamer Fortnite Mikukáš Adventure', 1470, 956)
+ 
+# Začni vykreslovať snímky v cykle (v predvolenej rýchlosti 60fps)
+should_quit = False
+while not should_quit:
+ fill(1,1,1)
+    # Načítaj udalosti pre aktuálnu snímku
+for event in poll_events():
+        # Napríklad ak hráč spustí CloseEvent
+        # prestaň ďalej vykreslovať snímky a zatvor okno 
+        if type(event) is CloseEvent:
+            should_quit = True
+    ###
+    # Tu patrí logika hry, ktorá na obrazovku niečo vykreslí
+    ###
+ 
+    # Pokračuj na ďalšiu snímku (a všetko opať prekresli)
+    next_frame()
+ 
+close_window()
