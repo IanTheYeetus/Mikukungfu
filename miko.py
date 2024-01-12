98044e4fb4051c23c3cb0c0a4a66e1e5c80db5e4
jakubklabnik <klabnik.jakub@gmail.com>
2024-01-12 10:10:57 +0100

    ide to

--- miko.py
+++ miko.py
@@ -5,7 +5,7 @@ open_window('Epic Gamer Fortnite Mikukáš Adventure', 1470, 956)
 # Začni vykreslovať snímky v cykle (v predvolenej rýchlosti 60fps)
 should_quit = False
 while not should_quit:
-    fill(1,1,1)
+    fill(0,0,0)
     # Načítaj udalosti pre aktuálnu snímku
     for event in poll_events():
         # Napríklad ak hráč spustí CloseEvent
@@ -14,6 +14,8 @@ while not should_quit:
             should_quit = True
     ###
     # Tu patrí logika hry, ktorá na obrazovku niečo vykreslí
+    
+    draw_text("Fortnite", 'Times New Roman', 50, (200, 200))
     ###
  
     # Pokračuj na ďalšiu snímku (a všetko opať prekresli)
